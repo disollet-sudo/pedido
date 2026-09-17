2d1a39ad27d10675f59f41ff7c028532e06acbf6
disollet-sudo <disollet@gmail.com>
2026-09-17 08:44:47 -0300

    Update 01-estado-global.js

--- js/01-estado-global.js
+++ js/01-estado-global.js
@@ -8,7 +8,7 @@
    Este arquivo precisa ser o PRIMEIRO <script> carregado no index.html.
    ============================================= */
 
-const URL_GOOGLE_SCRIPT = "https://script.google.com/macros/s/AKfycbw1OxgUj1Fudekl6QVihqIvipN9zGkwrfn81T6S6ntkh1mAc1MpxVC1xzW2i0QNewxgqQ/exec";
+const URL_GOOGLE_SCRIPT = "https://script.google.com/macros/s/AKfycbzFX0KDlHR7I7tICIifSoLJOJ2u6LlYPy0IyZfD-DKxrwZOMqUZEcWsO3YuXjQ-NT9H5w/exec";
 
 // --- ESTADO GLOBAL PRINCIPAL ---
 let PRODUTOS = [];
